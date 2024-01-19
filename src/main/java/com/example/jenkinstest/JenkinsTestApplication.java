46b849e4e66de14b2c84cb3797a21ce37f52aed5
Anh Hoang The <hoangtheanhc2lp@gmail.com>
2024-01-19 23:17:58 +0700

    oke

--- src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsTestApplication {
 
 
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
